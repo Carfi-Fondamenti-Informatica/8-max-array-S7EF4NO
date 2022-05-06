f80fdc5d58295f8ac609e9ee45612a917c2ea198
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-05-07 00:39:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "libreria.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main()
+{
+    int d;
+
+    cout<<"Inserisci la dimensione del vettore:";
+    cin>>d;
+    float p[d];
+    Init(p, d);
+
+    for(int i=0;i<d;i++)
+    {
+        cin>>p[i];
+    }
+
+    cout<<Max( p, d);
+    return 0;
 }
