4cbbd752359d1a6d4ac483cbfb69308fd35b78fd
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-05-07 00:39:25 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
 
+#ifndef ARRAY_ESERCITAZIONI_LIBRERIA_H
+#define ARRAY_ESERCITAZIONI_LIBRERIA_H
+
+
+float Max( float [], int  );
+void Init(float [],int );
+
+#endif //ARRAY_ESERCITAZIONI_LIBRERIA_H
+
