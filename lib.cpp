3fbc94502f2be56d1f5cda17d808df0cd595655f
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-05-07 00:38:42 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
 
+#include "libreria.h"
+
+void Init(float p[],int d)
+{
+    for(int i=0;i<d;i++)
+    {
+        p[i]=0;
+    }
+}
+
+float Max( float p[], int d)
+{
+    float temporaneo=p[0];
+
+    for(int i=1; i<d;i++)
+    {
+        if (p[i] > temporaneo)
+            temporaneo = p[i];
+    }
+    return temporaneo;
+}
